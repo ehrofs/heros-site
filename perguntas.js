ca775de240d2fbec4e489f6517a7d4746f4bdfa9
ehrofs <125662135+ehrofs@users.noreply.github.com>
2024-09-11 16:12:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,101 @@
+export const perguntas = [
+    {
+        enunciado: "Primeiro. De que lado você está?",
+        alternativas: [
+            {
+                texto: "Alexandre de Moraes",
+                afirmacao: [
+                    "Depois de muito tempo tentando decidir, você escolhe ser contra o X.",
+                    "Você inicia determinado em defender o Lord V. dos bilionários,"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Elon Musk",
+                afirmacao: [
+                    "Assim que você nota que vai perder sua conta de 2 seguidoes no X, você decide segui o bilionário.",
+                    "Ao lembrar que Elon Musk é foda, você vai o defender."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Como ganhar mais dinheiro do bilionário e reduzir a oposição.",
+        alternativas: [
+            {
+                texto: "Multar a Tesla",
+                afirmacao: [
+                    "e como solução para a situação que se encontra, decide que seria uma otima ideia multar a Tesla por ter carros tão lindos e caros.",
+                    "e como solução para a situação que se encotra, decide q multar a Tesla por demorara muito para os carros carregarem."
+                ],
+            },
+            {
+                texto: "Aumentar as multas do VPN",
+                afirmacao: [
+                    "você nota que uma otima opção pra reduzir a oposição seria proibir o VPN, aumentar as multas do uso para que o X seja lembrado com medo de ficar com o nome no serasa."
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "para melhorar a situação no Brasil, você pensa em...",
+        alternativas: [
+            {
+                texto: "aceitar os termos",
+                afirmacao: [
+                    "mas decide aceitar os termos que Lord V. mandou, pagar a multa milionaria, e seguir normalmente.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "recusar os termos",
+                afirmacao: [
+                    "e nota que para melhorar a situação do Brasil, é permanecer recusando a burocracia e zombando de Lord V.",
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Porem, Lord V. é muito pretencioso, e quer um pedido de desculpas em video.",
+        alternativas: [
+            {
+                texto: "não pedir",
+                afirmacao: [
+                    "só que Elon Musk é muito foda, então seria muita umilhação aceitar o video de desculpa.",
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "pedir",
+                afirmacao: [
+                    "só que o grande Musk não é orgulhoso, e para o bem do povo, decide ser formalmente correto, e aceitar a condição que Alinho mandou.",
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Ao recusar, Alexandre fica puto, e manda mais multa por Musk ser muito rico.",
+        alternativas: [
+            {
+                texto: "ver oq vai dar",
+                afirmacao: [
+                    "assim que Alexandre se junta ao IR, Musk é aniquilado de imposto,"
+                ],
+                proxima: 5,
+            },
+    
+        ]
+    },
+    {
+        enunciado: "Tudo que lhe restou, foi uma moto que esta sendo taxada por lhe dar rendimento",
+        alternativas: [
+            {
+                texto: "Lord V. ganha a batalha.",
+                afirmacao: [
+                    "que torna inviavel viajar para Marte."
+                ],
+            }
+        ]
+    },
+];
